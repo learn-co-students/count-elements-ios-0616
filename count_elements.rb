b228823ef72dcc91ae2696d67a1672f2d3d6e7ce
alex kalina <walterscott404@yandex.com>
2017-08-26 13:25:33 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,29 @@
+# require 'pry'
+# array = ['cat', 'dog', 'fish', 'fish']
 def count_elements(array)
+  new_hash = {}
+  count = 0
+  array.each do |x|
+    if new_hash.key? x
+    new_hash[x] = new_hash[x] + 1
+    #binding.pry
+  else
+    new_hash[x] = 1
+  end
+  end
+  new_hash
   # code goes here
 end
- 
+# count_elements(array)
+
+
+# def count_elements(array)
+#   count = Hash.new(0)
+#   array.each {|item| count[item] += 1 }
+#   count
+#
+#   # Fancy
+#   # array.each_with_object(Hash.new(0)) do |item, count|
+#   #   count[item] += 1
+#   # end
+# end
